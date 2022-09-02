0794eb25ff3d4b1e13483ade75e350f749cb23af
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-02 12:47:36 +0530

    initial commit

---
+++ keys.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose");
+
+module.exports={
+    MONGOURI:"mongodb+srv://mubarak:mubarak_123@cluster0.pvaka.mongodb.net/tribe?retryWrites=true&w=majority",
+    JWT_SECRET:"shajsljfadkjf"
+}
+
